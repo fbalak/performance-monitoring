ca23da1d88b1e16e980dc9e6dffc00ba33251cb9
Rohan Kanade <rkanade@redhat.com>
2016-11-28 22:52:46 +0530

    Enable travis for pep8 and coveralls
    tendrl-bug-id: performance_monitoring/3

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="tendrl-performance-monitoring",
+    version="1.1",
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*",
+                                    "tests"]),
+    namespace_packages=['tendrl'],
+    url="http://www.redhat.com",
+    author="Rohan Kanade.",
+    author_email="rkanade@redhat.com",
+    license="LGPL-2.1+",
+    zip_safe=False,
+    entry_points={
+        'console_scripts': ['tendrl-performance-monitoring = '
+                            'tendrl.performance_monitoring.manager.manager'
+                            ':main'
+                            ]
+    }
+)
