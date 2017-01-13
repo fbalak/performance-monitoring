62ee2b383b022c11a07df6d9e1064630f90cd2c5
anmolbabu <anmolbudugutta@gmail.com>
2017-01-13 14:19:44 +0530

    Incoporated comments
    tendrl-bug-id: Tendrl/performance_monitoring#15
    Signed-off-by: anmolbabu <anmolbudugutta@gmail.com>

--- tendrl/performance_monitoring/aggregator/summary.py
+++ tendrl/performance_monitoring/aggregator/summary.py
@@ -1,81 +1,34 @@
-import ast
 import datetime
-import etcd
-import json
 import logging
 import multiprocessing
 import re
-from tendrl.common.config import ConfigNotFound
-from tendrl.common.config import TendrlConfig
-from tendrl.common.etcdobj.etcdobj import Server as etcd_server
 from tendrl.performance_monitoring.exceptions \
     import TendrlPerformanceMonitoringException
-from tendrl.performance_monitoring.time_series_db.manager \
-    import TimeSeriesDBManager
 import time
 import urllib2
 
 LOG = logging.getLogger(__name__)
-config = TendrlConfig()
 
 
 class Summarise(multiprocessing.Process):
-    def __init__(self):
+    def __init__(self, persister_instance, timeSeriesDbManager):
         super(Summarise, self).__init__()
         self._complete = multiprocessing.Event()
-        etcd_kwargs = {
-            'port': int(config.get("common", "etcd_port")),
-            'host': config.get("common", "etcd_connection")
-        }
-        self.etcd_client = etcd_server(etcd_kwargs=etcd_kwargs).client
-
-    def get_node_name_from_id(self, node_id):
-        try:
-            node_name_path = '/nodes/%s/Node_context/fqdn' % node_id
-            return self.etcd_client.read(node_name_path).value
-        except (
-            ConfigNotFound,
-            etcd.EtcdKeyNotFound,
-            etcd.EtcdConnectionFailed,
-            ValueError,
-            SyntaxError,
-            etcd.EtcdException,
-            TypeError
-        ) as ex:
-            raise ex
-
-    def get_node_ids(self):
-        try:
-            nodes = self.etcd_client.read(
-                '/nodes',
-                recursive=True
-            )
-            node_ids = []
-            for child in nodes._children:
-                for node in child['nodes']:
-                    for _node in node['nodes']:
-                        if _node['key'].endswith('Node_context/node_id'):
-                            node_ids.append(_node['value'])
-            return node_ids
-        except etcd.EtcdKeyNotFound:
-            return []
-        except (
-            ConfigNotFound,
-            etcd.EtcdConnectionFailed,
-            ValueError,
-            SyntaxError,
-            TypeError
-        ) as ex:
-            LOG.error(
-                'Failed to get nodes from etcd. Error %s' % str(ex),
-                exc_info=True
-            )
-            raise TendrlPerformanceMonitoringException(ex)
+        self._persister = persister_instance
+        self.time_series_db_manager = timeSeriesDbManager
 
     def get_latest_stat(self, node, resource):
+        '''
+            Get latest stats of resource as in param resource
+            Keyword arguments:
+            node: The id of node of which the latest stats are required
+            resource: The name of resource
+            Returns:
+            Latest stat of float type
+        '''
         try:
-            node_name = self.get_node_name_from_id(node)
-            stats = TimeSeriesDBManager().get_plugin().get_metric_stats(
+            node_name = self._persister.get_node_name_from_id(node)
+            stats = self.time_series_db_manager.get_plugin().get_metric_stats(
                 node_name,
                 resource,
                 'latest'
@@ -91,9 +44,17 @@ class Summarise(multiprocessing.Process):
             raise TendrlPerformanceMonitoringException(ex)
 
     def get_latest_stats(self, node, resource):
+        '''
+            Get latest stats of resources matching wild cards in param resource
+            Keyword arguments:
+            node: The id of node of which the latest stats are required
+            resource: The pattern of resource name including wild cards
+            Returns:
+            Array of latest stats
+        '''
         try:
-            node_name = self.get_node_name_from_id(node)
-            stats = TimeSeriesDBManager().get_plugin().get_metric_stats(
+            node_name = self._persister.get_node_name_from_id(node)
+            stats = self.time_series_db_manager.get_plugin().get_metric_stats(
                 node_name,
                 resource,
                 'latest'
@@ -116,7 +77,7 @@ class Summarise(multiprocessing.Process):
                 'updated_at': datetime.datetime.now().isoformat()
             }
         except TendrlPerformanceMonitoringException:
-            # Exception already handle_data
+            # Exception already handled
             return None
 
     def get_net_host_memory_utilization(self, node):
@@ -130,7 +91,7 @@ class Summarise(multiprocessing.Process):
                 'updated_at': datetime.datetime.now().isoformat()
             }
         except TendrlPerformanceMonitoringException:
-            # Exception already handle_data
+            # Exception already handled
             return None
 
     def get_net_storage_utilization(self, node):
@@ -151,78 +112,38 @@ class Summarise(multiprocessing.Process):
                 'updated_at': datetime.datetime.now().isoformat()
             }
         except TendrlPerformanceMonitoringException:
-            # Exception already handle_data
+            # Exception already handled
             return None
-
-    def get_alerts(self, node_ids):
-        alerts_arr = []
-        try:
-            alerts = self.etcd_client.read('/alerts', recursive=True)
-        except etcd.EtcdKeyNotFound:
-            return alerts_arr
-        except (
-            etcd.EtcdConnectionFailed,
-            ValueError,
-            SyntaxError,
-            TypeError
-        ) as ex:
-            LOG.error(
-                'Failed to fetch alerts. Error %s' % str(ex),
-                exc_info=True
-            )
-            return None
-        for child in alerts._children:
-            alerts_arr.append(json.loads(child['value']))
-        if node_ids is not None:
-            filtered_alerts = []
-            for alert in alerts_arr:
-                if alert['node_id'] in node_ids:
-                    filtered_alerts.append(alert)
-            return filtered_alerts
-        return alerts_arr
 
     def calculate_host_summary(self, node):
-        summary = {}
         try:
-            summary = ast.literal_eval(
-                self.etcd_client.read(
-                    '/monitoring/summary/%s' % node
-                ).value
-            )
-        except etcd.EtcdKeyNotFound:
-            pass
-        except (
-            etcd.EtcdConnectionFailed,
-            ValueError,
-            SyntaxError,
-            TypeError
-        ) as ex:
+            summary = self._persister.get_node_summary(node)
+            if not summary:
+                return
+            net_cpu_utilization = self.get_net_host_cpu_utilization(node)
+            if net_cpu_utilization is not None:
+                summary['cpu'] = net_cpu_utilization
+            net_memory_utilization = self.get_net_host_memory_utilization(node)
+            if net_memory_utilization is not None:
+                summary['memory'] = net_memory_utilization
+            net_storage_utilization = self.get_net_storage_utilization(node)
+            if net_storage_utilization is not None:
+                summary['storage'] = net_storage_utilization
+            alert_count = len(self._persister.get_alerts(node))
+            if alert_count is not None:
+                summary['alert_cnt'] = alert_count
+            if summary:
+                self._persister.save_node_summary(summary, node)
+        except TendrlPerformanceMonitoringException as ex:
             LOG.error(
-                'Failed to fetch previously stored summary for %s.'
-                'Error %s' % (node, ex),
-                exc_info=True
-            )
-            return
-        net_cpu_utilization = self.get_net_host_cpu_utilization(node)
-        if net_cpu_utilization is not None:
-            summary['cpu'] = net_cpu_utilization
-        net_memory_utilization = self.get_net_host_memory_utilization(node)
-        if net_memory_utilization is not None:
-            summary['memory'] = net_memory_utilization
-        net_storage_utilization = self.get_net_storage_utilization(node)
-        if net_storage_utilization is not None:
-            summary['storage'] = net_storage_utilization
-        alert_count = len(self.get_alerts(node))
-        if alert_count is not None:
-            summary['alert_cnt'] = alert_count
-        if summary:
-            self.etcd_client.write(
-                '/monitoring/summary/%s' % node,
-                summary
+                'Exception %s caught while calculating summary for node %s' % (
+                    str(ex),
+                    node
+                ), exc_info=True
             )
 
     def calculate_host_summaries(self):
-        nodes = self.get_node_ids()
+        nodes = self._persister.get_node_ids()
         for node in nodes:
             self.calculate_host_summary(node)
 
@@ -233,3 +154,4 @@ class Summarise(multiprocessing.Process):
 
     def stop(self):
         self._complete.set()
+
