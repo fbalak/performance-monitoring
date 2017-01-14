b7a9d6440a5f9dfa33bcf8610707c2f375aeaf5a
anmolbabu <anmolbudugutta@gmail.com>
2017-01-15 00:46:23 +0530

    Fix UT
    tendrl-bug-id: Tendrl/performance_monitoring#7
    Signed-off-by: anmolbabu <anmolbudugutta@gmail.com>

--- tendrl/performance_monitoring/tests/test_api_manager.py
+++ tendrl/performance_monitoring/tests/test_api_manager.py
@@ -1,26 +1,54 @@
+from ConfigParser import SafeConfigParser
 from mock import MagicMock
 import multiprocessing
 import sys
 from tendrl.performance_monitoring.api.manager import APIManager
-from tendrl.performance_monitoring.api.manager import config
+from tendrl.performance_monitoring.persistence.persister \
+    import PerformanceMonitoringEtcdPersister
+from tendrl.performance_monitoring.time_series_db.manager \
+    import TimeSeriesDBManager
 sys.modules['tendrl.common.config'] = MagicMock()
 sys.modules['tendrl.common.log'] = MagicMock()
 del sys.modules['tendrl.common.config']
 
 
 class TestManager(object):
+
+    def get_persister(self):
+        cParser = SafeConfigParser()
+        cParser.add_section('commons')
+        cParser.set('commons', 'etcd_connection', '0.0.0.0')
+        cParser.set('commons', 'etcd_port', '2379')
+        return PerformanceMonitoringEtcdPersister(cParser)
+
+    def get_time_series_db_manager(self):
+        cParser = SafeConfigParser()
+        cParser.add_section('commons')
+        cParser.set('commons', 'etcd_connection', '0.0.0.0')
+        cParser.set('commons', 'etcd_port', '2379')
+        cParser.add_section('time_series')
+        cParser.set('time_series', 'time_series_db_server', '0.0.0.0')
+        cParser.set('time_series', 'time_series_db_port', '80')
+        return TimeSeriesDBManager(cParser, 'graphite')
+
     def test_manager_constructor(self, monkeypatch):
-        def mock_config(package, parameter):
-            if parameter == "performance_server_interface":
-                return '0.0.0.0'
-        monkeypatch.setattr(config, 'get', mock_config)
-        manager = APIManager()
+        manager = APIManager(
+            '0.0.0.0',
+            '5000',
+            self.get_persister(),
+            self.get_time_series_db_manager()
+        )
         assert manager.host == '0.0.0.0'
         assert isinstance(manager, APIManager)
         assert isinstance(manager._complete, multiprocessing.synchronize.Event)
 
     def test_manager_stop(self, monkeypatch):
-        manager = APIManager()
+        manager = APIManager(
+            '0.0.0.0',
+            '5000',
+            self.get_persister(),
+            self.get_time_series_db_manager()
+        )
 
         def mock_stop():
             return
@@ -30,7 +58,12 @@ class TestManager(object):
         assert True
 
     def test_manager_start(self, monkeypatch):
-        manager = APIManager()
+        manager = APIManager(
+            '0.0.0.0',
+            '5000',
+            self.get_persister(),
+            self.get_time_series_db_manager()
+        )
 
         def mock_start():
             return
