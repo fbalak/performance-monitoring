3e957003f5f9bcdd2ae795bc1aa2a32fab8258a3
anmolbabu <anmolbudugutta@gmail.com>
2017-01-15 14:32:04 +0530

    Fix travis failures
    tendrl-bug-id: Tendrl/performance_monitoring#7
    Signed-off-by: anmolbabu <anmolbudugutta@gmail.com>

--- tendrl/performance_monitoring/tests/test_time_series_db.py
+++ tendrl/performance_monitoring/tests/test_time_series_db.py
@@ -2,10 +2,10 @@ from ConfigParser import SafeConfigParser
 from mock import MagicMock
 import sys
 sys.modules['tendrl.common.log'] = MagicMock()
+from tendrl.performance_monitoring.time_series_db.dbplugins.graphite \
+    import GraphitePlugin
 from tendrl.performance_monitoring.time_series_db.manager \
     import TimeSeriesDBManager
-#from tendrl.performance_monitoring.time_series_db.dbplugins.graphite \
-#    import GraphitePlugin
 del sys.modules['tendrl.common.log']
 
 
@@ -17,21 +17,16 @@ class TestTimeSeriesDbManager(object):
         cParser.set('commons', 'etcd_port', '2379')
         cParser.add_section('time_series')
         cParser.set('time_series', 'time_series_db', db_name)
+        cParser.set('time_series', 'time_series_db_server', '0.0.0.0')
+        cParser.set('time_series', 'time_series_db_port', '80')
         return cParser
 
-#    def test_time_series_db_manager_sucess(self, monkeypatch):
-#        time_series_db_manager = TimeSeriesDBManager(
-#            self.get_mock_config('graphite'),
-#            'graphite'
-#        )
-#        assert isinstance(
-#            time_series_db_manager.get_plugin(),
-#            GraphitePlugin
-#        )
-
-    def test_time_series_db_manager_failed(self, monkeypatch):
+    def test_time_series_db_manager_sucess(self, monkeypatch):
         time_series_db_manager = TimeSeriesDBManager(
-            self.get_mock_config('rhq'),
-            'rhq'
+            self.get_mock_config('graphite'),
+            'graphite'
         )
-        assert time_series_db_manager.get_plugin() is None
+        assert isinstance(
+            time_series_db_manager.get_plugin(),
+            GraphitePlugin
+        )
