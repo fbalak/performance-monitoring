8edd242dcd1e55b4461b9ebb4487abf1651cc474
anmolbabu <anmolbudugutta@gmail.com>
2017-01-15 12:58:19 +0530

    Fix UT
    tendrl-bug-id: Tendrl/performance_monitoring#7
    Signed-off-by: anmolbabu <anmolbudugutta@gmail.com>

--- tendrl/performance_monitoring/tests/test_time_series_db.py
+++ tendrl/performance_monitoring/tests/test_time_series_db.py
@@ -1,31 +1,37 @@
+from ConfigParser import SafeConfigParser
 from mock import MagicMock
 import sys
-sys.modules['tendrl.common.config'] = MagicMock()
 sys.modules['tendrl.common.log'] = MagicMock()
 from tendrl.performance_monitoring.time_series_db.manager \
-    import config
-from tendrl.performance_monitoring.time_series_db.manager \
     import TimeSeriesDBManager
-# from tendrl.performance_monitoring.time_series_db.dbplugins.graphite \
+#from tendrl.performance_monitoring.time_series_db.dbplugins.graphite \
 #    import GraphitePlugin
-del sys.modules['tendrl.common.config']
 del sys.modules['tendrl.common.log']
 
 
 class TestTimeSeriesDbManager(object):
-    #    def test_time_series_db_manager_sucess(self, monkeypatch):
-    #        def mock_config_get(package, parameter):
-    #            if parameter == "time_series_db":
-    #                return 'graphite'
-    #        monkeypatch.setattr(config, 'get', mock_config_get)
-    #        time_series_db_manager = TimeSeriesDBManager()
-    #        assert isinstance(time_series_db_manager.get_plugin(),
-    #        	GraphitePlugin)
+    def get_mock_config(self, db_name):
+        cParser = SafeConfigParser()
+        cParser.add_section('commons')
+        cParser.set('commons', 'etcd_connection', '0.0.0.0')
+        cParser.set('commons', 'etcd_port', '2379')
+        cParser.add_section('time_series')
+        cParser.set('time_series', 'time_series_db', db_name)
+        return cParser
+
+#    def test_time_series_db_manager_sucess(self, monkeypatch):
+#        time_series_db_manager = TimeSeriesDBManager(
+#            self.get_mock_config('graphite'),
+#            'graphite'
+#        )
+#        assert isinstance(
+#            time_series_db_manager.get_plugin(),
+#            GraphitePlugin
+#        )
 
     def test_time_series_db_manager_failed(self, monkeypatch):
-        def mock_config_get_non_existing_plugin(package, parameter):
-            if parameter == "time_series_db":
-                return 'rhq'
-        monkeypatch.setattr(config, 'get', mock_config_get_non_existing_plugin)
-        time_series_db_manager = TimeSeriesDBManager()
+        time_series_db_manager = TimeSeriesDBManager(
+            self.get_mock_config('rhq'),
+            'rhq'
+        )
         assert time_series_db_manager.get_plugin() is None
