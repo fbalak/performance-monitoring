07c3fa08189832b23bda00fc60fbcbc3491f4775
anmolbabu <anmolbudugutta@gmail.com>
2017-01-15 14:13:33 +0530

    Fix UTs
    tendrl-bug-id: Tendrl/performance_monitoring#7, Tendrl/performance_monitoring#8
    Signed-off-by: anmolbabu <anmolbudugutta@gmail.com>

--- tendrl/performance_monitoring/tests/test_manager.py
+++ tendrl/performance_monitoring/tests/test_manager.py
@@ -1,21 +1,23 @@
 from mock import MagicMock
 from multiprocessing import queues
 import sys
-sys.modules['tendrl.common.config'] = MagicMock()
-sys.modules['tendrl.common.log'] = MagicMock()
-from tendrl.common.etcdobj.etcdobj import Server as etcd_server
+sys.modules['tendrl.commons.config'] = MagicMock()
+sys.modules['tendrl.commons.log'] = MagicMock()
+from tendrl.performance_monitoring.aggregator.summary import Summarise
 from tendrl.performance_monitoring.api.manager import APIManager
 from tendrl.performance_monitoring.configure.configurator import Configurator
 from tendrl.performance_monitoring.configure.configure_monitoring \
     import ConfigureMonitoring
 from tendrl.performance_monitoring.defaults.manager import \
     DefaultsManager
-from tendrl.performance_monitoring.manager \
+from tendrl.performance_monitoring.manager.manager \
     import config
-from tendrl.performance_monitoring.manager \
+from tendrl.performance_monitoring.manager.manager \
     import TendrlPerformanceManager
-del sys.modules['tendrl.common.config']
-del sys.modules['tendrl.common.log']
+from tendrl.performance_monitoring.persistence.persister \
+    import PerformanceMonitoringEtcdPersister
+del sys.modules['tendrl.commons.config']
+del sys.modules['tendrl.commons.log']
 
 
 class TestManager(object):
@@ -29,12 +31,16 @@ class TestManager(object):
 
         monkeypatch.setattr(config, 'get', mock_config)
         manager = TendrlPerformanceManager()
-        assert isinstance(manager.etcd_server, etcd_server)
+        assert isinstance(
+            manager.persister,
+            PerformanceMonitoringEtcdPersister
+        )
         assert isinstance(manager.api_manager, APIManager)
         assert isinstance(manager.defaults_manager, DefaultsManager)
         assert isinstance(manager.configurator_queue, queues.Queue)
         assert isinstance(manager.configure_monitoring, ConfigureMonitoring)
         assert isinstance(manager.configurator, Configurator)
+        assert isinstance(manager.node_summariser, )
 
     def test_start(self, monkeypatch):
 
