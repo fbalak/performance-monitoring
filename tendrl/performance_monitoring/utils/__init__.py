06822d2024d1219dd2b5ceb04a60f97a7d16d712
anmolbabu <anmolbudugutta@gmail.com>
2017-04-01 19:53:47 +0530

    Fixes
    Signed-off-by: anmolbabu <anmolbudugutta@gmail.com>

--- tendrl/performance_monitoring/utils/__init__.py
+++ tendrl/performance_monitoring/utils/__init__.py
@@ -56,3 +56,16 @@ def initiate_config_generation(node_det):
                 str(ex)
             )
         )
+
+
+# this function can return json for any etcd key
+def read(key):
+    result = {}
+    job = NS.etcd_orm.client.read(key)
+    if hasattr(job, 'leaves'):
+        for item in job.leaves:
+            if item.dir is True:
+                result[item.key.split("/")[-1]] = read(item.key)
+            else:
+                result[item.key.split("/")[-1]] = item.value
+    return result
